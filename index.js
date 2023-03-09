29df97fc711fd6edf119d6947274f2a396391c0e
itsparasbisht <iamparasbisht@gmail.com>
2023-03-09 20:29:32 +0530

    express router setup

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
+// routes ---------------
+const ghgEmissionsRoute = require("./routes/ghg-emissions");
+
 app.get("/", (req, res) => {
   res.json({
     message: "base route of the application",
   });
 });
+
+app.use("/ghg-emissions", ghgEmissionsRoute);
 
 app.listen(5000, () => {
   console.log(`Listening on port ${PORT}...`);
